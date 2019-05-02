de192b157d21f195af5091205132ba280dede312
Skyl3r <kpereksta@gmail.com>
2019-05-02 10:02:43 -0400

    Added rank tracking

--- TimedEvents.php
+++ TimedEvents.php
@@ -2,6 +2,7 @@
 
 use DiDom\Document;
 use DiDom\Query;
+require_once 'Rank.php'
 
 $TimedEvent = [
 
@@ -32,5 +33,43 @@ $TimedEvent = [
 		}
 
 	},
+
+	'Check Ranks'		=> function(&$bucket) {
+		Global $Ranks;
+
+		$profiles	= [];
+		$ranks		= [];
+
+		if(file_exists('ranks.txt'))
+			$ranks = unserialize(file_get_contents('ranks.txt'));
+		
+		if(file_exists('profiles.txt'))
+			$profiles = unserialize(file_get_contents('profiles.txt'));
+
+		foreach($profiles as $profile) {
+			$comparisonRanks = new Rank();
+			$comparisonRanks->rocketLeagueName = $profile;
+			$comparisonRanks->getRank();
+
+			if(key_exists($profile[0], $ranks)) {
+				
+				foreach($ranks[$profile[0]]->ranks as $rank => &$value) {
+					if($comparisonRanks[$rank] < $value) {
+						$bucket->getSource()->say("Back to the drawing board " . $profile[0] . "! Demoted in " . $rank . " to " . $Ranks[$comparisonRanks[$rank]]); 
+					}
+					if($comparisonRanks[$rank] > $value) {
+						$bucket->getSource()->say("Good work ". $profile[0] . "! Promoted in " . $rank . " to " . $Ranks[$comparisonRanks[$rank]]);
+					}
+					$value = $comparisonRanks[$rank];
+				}
+
+			} else {
+				$ranks[$profile[0]] = $comparisonRanks;
+			}
+		}
+
+		file_put_contents('ranks.txt', serialize($ranks));
+
+	}
 
 ];
